60ff8a60e59fc4bbe092a85a796575abcbbfa47e
IlazCode <gehbka@mail.ru>
2023-12-23 07:58:52 +0300

    Переделана авторизация на глобальную переменную Глобальная реструктуризация кода под новую авторизацию

--- main.py
+++ main.py
@@ -1,42 +1,25 @@
 import logging
-from logging import FileHandler
-import uuid
 from aiogram import Bot, Dispatcher, types, executor
 import httpx
-import time
 import json
-from typing import List, Dict, Union
 import os
+import logging
+from logging import FileHandler
+from typing import List, Dict, Union
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.handler import CancelHandler
 from aiogram.dispatcher.middlewares import BaseMiddleware
+import uuid
 
-
-# Telegram bot's token
+# Загрузка конфигурации бота из файла
 with open("config.json", encoding="utf-8") as file_handler:
     CONFIG = json.load(file_handler)
 
-# Замените 'YOUR_BOT_TOKEN' на токен вашего бота
+# Получение токена бота из конфигурации
 BOT_TOKEN = CONFIG["tg_token"]
 
+# Список пользователей с админским доступом
 users = CONFIG["admin_tg_id"]
-
-class AccessMiddleware(BaseMiddleware):
-    """
-    Функция, которая вызывается перед обработкой сообщения. Она проверяет, имеет ли пользователь доступ к использованию бота, и вызывает исключение CancelHandler, если доступа нет.
-    """
-    async def on_pre_process_message(self, message: types.Message, data: dict):
-        if message.from_user.id not in users:
-            await message.reply("У вас нет доступа к использованию бота.")
-            raise CancelHandler()
-
-    """
-    Функция, Когда пользователь нажимает на кнопку в чате или встроенной клавиатуре. Она проверяет, имеет ли пользователь доступ к использованию бота.
-    """
-    async def on_pre_process_callback_query(self, callback_query: types.CallbackQuery, data: dict):
-        if callback_query.from_user.id not in users:
-            await callback_query.answer("У вас нет доступа к использованию бота.")
-            raise CancelHandler()
 
 # Имя файла для сохранения логов
 # LOG_FILE = 'bot_log.txt'
@@ -52,24 +35,40 @@ logging.basicConfig(level=logging.INFO)
 # Инициализация бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
-# Добавляем middleware к диспетчеру
+
+# Middleware для проверки доступа пользователей
+class AccessMiddleware(BaseMiddleware):
+    async def on_pre_process_message(self, message: types.Message, data: dict):
+        if message.from_user.id not in users:
+            await message.reply("У вас нет доступа к использованию бота.")
+            raise CancelHandler()
+
+    async def on_pre_process_callback_query(self, callback_query: types.CallbackQuery, data: dict):
+        if callback_query.from_user.id not in users:
+            await callback_query.answer("У вас нет доступа к использованию бота.")
+            raise CancelHandler()
+
+# Добавление middleware для проверки доступа
 dp.middleware.setup(AccessMiddleware())
 
-# Замените 'YOUR_AUTHORIZATION_DATA' на ваши фактические авторизационные данные
+# Данные для авторизации
 AUTHORIZATION_DATA = CONFIG["key"]
+
+# Глобальная переменная для хранения токена
+access_token = None
 
 # Функция для формирования уникального идентификатора запроса
 def generate_rquid() -> str:
     return str(uuid.uuid4())
 
+# Функция для авторизации
+async def authorize() -> None:
+    global access_token
 
-# Функция для выполнения запроса авторизации
-
-async def authorize(rquid: str) -> dict:
     auth_url = 'https://ngw.devices.sberbank.ru:9443/api/v2/oauth'
     headers = {
         'Authorization': f'Bearer {AUTHORIZATION_DATA}',
-        'RqUID': rquid,
+        'RqUID': generate_rquid(),
         'Content-Type': 'application/x-www-form-urlencoded',
     }
     data = {'scope': 'GIGACHAT_API_PERS'}
@@ -77,32 +76,13 @@ async def authorize(rquid: str) -> dict:
     async with httpx.AsyncClient(verify=False) as client:
         response = await client.post(auth_url, headers=headers, data=data)
 
-    return response.json(), response.status_code
+    if response.status_code == 200:
+        auth_data = response.json()
+        access_token = auth_data.get('access_token', '')
+    else:
+        print("Ошибка авторизации")
 
-
-# Функция для проверки срока действия токена
-# def is_token_expired(expires_at: int) -> bool:
-#     current_time = int(time.time() * 1000)  # Текущее время в миллисекундах
-#     return expires_at < current_time
-
-
-# Функция для сохранения истории сообщений в файл
-def save_history(user_id: int, messages: List[Dict[str, Union[str, int]]]):
-    history_file = f'history_{user_id}.json'
-    try:
-        with open(history_file, 'r') as file:
-            history = json.load(file)
-    except FileNotFoundError:
-        history = []
-
-    history.extend(messages)
-
-    with open(history_file, 'w') as file:
-        json.dump(history, file)
-
-
-# Функция для отправки сообщений от пользователя в API
-
+# Функция для отправки сообщений пользователя в API
 async def send_user_messages(access_token: str, messages: List[Dict[str, Union[str, int]]], timeout: int = 30):
     api_url = 'https://gigachat.devices.sberbank.ru/api/v1/chat/completions'
     headers = {
@@ -120,59 +100,52 @@ async def send_user_messages(access_token: str, messages: List[Dict[str, Union[s
 
     return response.json(), response.status_code
 
-
-
-# Функция для обработки ответа API на отправку сообщений от пользователя
+# Функция для обработки сообщений пользователя из API
 async def handle_user_messages(message: types.Message, access_token: str, user_id: int):
-    # Генерируем уникальный идентификатор запроса
     rquid = generate_rquid()
-
-    # Выполняем запрос отправки сообщений от пользователя
     messages_to_api = [{'role': 'user', 'content': message.text}]
     api_response, status_code = await send_user_messages(access_token, messages_to_api)
 
     if status_code == 200:
-        # Забираем значение "message" из ответа API
         assistant_message = api_response.get('choices', [])[0].get('message', {}).get('content', '')
         messages_out_api = [{'role': 'assistant', 'content': assistant_message}]
 
-        # Сохраняем историю сообщений
         save_history(user_id, messages_to_api)
         save_history(user_id, messages_out_api)
 
-        # Отправляем ответ от API пользователю
         await message.reply(assistant_message)
     elif status_code == 401:
-        error_message = api_response.get('message', '')
-        if "Token has expired" in error_message:
-            # Переавторизуемся в случае истечения срока действия токена
-            await handle_authorization(message)
-            # Повторно отправляем сообщения от пользователя
-            await handle_user_messages(message, access_token, user_id)
-        else:
-            await message.reply("Ошибка отправки сообщения. Неверные данные авторизации.")
+        await authorize()
+        await handle_user_messages(message, access_token, user_id)
     else:
         await message.reply("Ошибка отправки сообщения. Неизвестная ошибка.")
 
+# Функция для сохранения истории сообщений
+def save_history(user_id: int, messages: List[Dict[str, Union[str, int]]]):
+    history_file = f'history_{user_id}.json'
+    try:
+        with open(history_file, 'r') as file:
+            history = json.load(file)
+    except FileNotFoundError:
+        history = []
 
-# Обработка команды /start
+    history.extend(messages)
 
+    with open(history_file, 'w') as file:
+        json.dump(history, file)
+
+# Обработчик команды /start
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     await message.reply("Чем могу помочь?")
 
-
-# Обработка команды /reset
-
+# Обработчик команды /reset
 @dp.message_handler(commands=['reset'])
 async def reset_command(message: types.Message):
-    # Получаем уникальный идентификатор пользователя
     user_id = message.from_user.id
-    # Создаем имя файла для данного пользователя
     history_file = f'history_{user_id}.json'
 
     try:
-        # Пытаемся удалить файл истории чата
         os.remove(history_file)
         await message.reply("История чата успешно удалена.")
     except FileNotFoundError:
@@ -180,23 +153,15 @@ async def reset_command(message: types.Message):
     except Exception as e:
         await message.reply(f"Произошла ошибка при удалении истории чата: {str(e)}")
 
-
-# Обработка сообщений от пользователя
+# Обработчик всех остальных сообщений
 @dp.message_handler()
 async def handle_user_message(message: types.Message):
-    # Получаем уникальный идентификатор пользователя
+    if access_token is None:
+        await authorize()  # Авторизуемся, если токен отсутствует
     user_id = message.from_user.id
-    # Получаем токен доступа из функции авторизации
-    auth_response, _ = await authorize(generate_rquid())
-    access_token = auth_response.get('access_token', '')
-
-    if not access_token:
-        await message.reply("Ошибка получения токена доступа. Пожалуйста, переавторизуйтесь.")
-        return
 
     # Обрабатываем сообщения от пользователя
     await handle_user_messages(message, access_token, user_id)
-
 
 # Запуск бота
 if __name__ == '__main__':
